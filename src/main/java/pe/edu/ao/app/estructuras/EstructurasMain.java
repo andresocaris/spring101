3faa349ee448c0546dc31464247de8ab41f21af0
Andres Ocaris <andresocaris@MacBook-Air-de-Andres.local>
2021-09-03 21:50:03 -0500

    restructura

--- src/main/java/pe/edu/ao/app/estructuras/EstructurasMain.java
+++ src/main/java/pe/edu/ao/app/estructuras/EstructurasMain.java
@@ -9,10 +9,14 @@ public class EstructurasMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Andres<String,String> M = new Andres<String,String>();
+			
 		M.put2("hola", "andres");
 		M.put2("peru", "***");
 		M.put2("carpeta","reloj");
+		
+		
 		System.out.println(M.get2("hola"));
 		System.out.println(M.get2("peru"));
 		
@@ -34,7 +38,6 @@ public class EstructurasMain {
 		// sale de un HashMap
 		HashSet<Integer> hashSet = new HashSet();
 		LinkedHashMap<String,String> linked=new LinkedHashMap<String,String>();
-		
 		
 		
 		
