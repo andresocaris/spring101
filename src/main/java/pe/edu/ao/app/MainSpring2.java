01234eff40f1891e9794be1fd289f5dfc3a454a8
Andres Ocaris <andresocaris@MacBook-Air-de-Andres.local>
2021-09-04 23:43:34 -0500

    prueba de spring con archivo xml

---
+++ src/main/java/pe/edu/ao/app/MainSpring2.java
@@ -0,0 +1,9 @@
+package pe.edu.ao.app;
+
+public class MainSpring2 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/main/java/pe/edu/ao/app/persona/Persona.java
@@ -0,0 +1,8 @@
+package pe.edu.ao.app.persona;
+
+public class Persona {
+	String nombre;
+	public Persona(String nombre){
+		this.nombre=nombre;
+	}
+}
